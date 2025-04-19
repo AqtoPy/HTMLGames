dafe9a3c75032c7700efcfbb9df487772fca8123
AqtoPy <rekikreki66@gmail.com>
2025-04-19 20:58:43 +0300

    Create CaseBattleURL.gd

---
+++ CaseBattleURL.gd
@@ -0,0 +1,1 @@
+file:///storage/emulated/0/Android/data/com.teejay.trebedit/files/TrebEdit%20user%20files/%D0%9F%D1%80%D0%B8%D0%BC%D0%B5%D1%80%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0%20-Acme/casebattle.html
